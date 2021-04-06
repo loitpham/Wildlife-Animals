08900ed6f4b4ba4133c189905e307531dd6bd114
Loi Pham <loitpham@gmail.com>
2021-04-06 13:47:56 -0700

    Added a cover image carousel on the Browse Page

---
+++ Wildlife Animals/Wildlife Animals/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Wildlife Animals
+//
+//  Created by Loi Pham on 4/6/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
