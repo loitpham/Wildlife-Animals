b9ff7cb01a5c321a6c1c638bf8f7c64e8736fa81
Loi Pham <loitpham@gmail.com>
2021-04-06 20:00:34 -0700

    Added a Video List View on the Watch Page and a Shuffle Button.

--- Wildlife Animals/Wildlife Animals/App/VideoListView.swift
+++ Wildlife Animals/Wildlife Animals/App/VideoListView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json").shuffled()
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    NavigationLink(
+                        destination: Text(item.name)) {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    }//: NAVIGATION-LINK
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Shuffle videos
+                        // Note:
+                        // shuffle(): a mutating func, shuffling in-place
+                        // shuffled(): returns a shuffled array
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION
     }
 }
 
+// MARK: PREVIEW
 struct VideoListView_Previews: PreviewProvider {
+    static var videos: [Video] = Bundle.main.decode("videos.json").shuffled()
     static var previews: some View {
-        VideoListView()
+        VideoListView(videos: videos)
             .previewDevice("iPhone 12 Pro Max")
     }
 }
---
+++ Wildlife Animals/Wildlife Animals/View/VideoListItemView.swift
@@ -0,0 +1,54 @@
+//
+//  VideoListItem.swift
+//  Wildlife Animals
+//
+//  Created by Loi Pham on 4/6/21.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    // MARK: - PROPERTIES
+    let video: Video
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: ZSTACK
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+        } //: HSTACK
+    }
+}
+
+// MARK: PREVIEW
+struct VideoListItem_Previews: PreviewProvider {
+    static let videos: [Video] = Bundle.main.decode("videos.json").shuffled()
+    static var previews: some View {
+        VideoListItemView(video: videos.first!)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
