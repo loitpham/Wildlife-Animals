b16a04b97ecc13067168f279cd85b64efe79f281
Loi Pham <loitpham@gmail.com>
2021-04-07 15:47:37 -0700

    Updated the Map View with latitude and longitude details and animation

---
+++ Wildlife Animals/Wildlife Animals/Model/LocationModel.swift
@@ -0,0 +1,23 @@
+//
+//  LocationModel.swift
+//  Wildlife Animals
+//
+//  Created by Loi Pham on 4/7/21.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+}
